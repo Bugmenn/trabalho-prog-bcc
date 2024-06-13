f0a624eeebdae3e15e22d80d1a85f19b859acb5b
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2024-06-13 16:58:20 -0300

    terminado kkkk

--- trabalho/src/batalhaNaval.java
+++ trabalho/src/batalhaNaval.java
@@ -8,18 +8,26 @@ public class batalhaNaval {
     }
 
     private batalhaNaval() {
-        Scanner s = new Scanner(System.in);
-
         String matriz[][] = CriarMatriz();
+        int qtAcertos = 0;
+        int qtJogadas = 0;
+        int fim[] = {qtAcertos, qtJogadas}; // valores para confimar o fim do jogo
 
-        Escrever(matriz);
+        boolean continuar;
+        
+        do {
+            Jogar(matriz, fim);
+            continuar = Fim(fim);
+            Escrever(matriz, continuar); // imprime o tabuleiro
+            System.out.println("Acertos: "+fim[0]+"  Jogadas: "+fim[1]);
+        } while (continuar);
     }
 
     private String[][] CriarMatriz() {
         // criação matriz 8x8
         String matriz[][] = new String[8][8];
 
-        // colocar a "água no tabuleiro"
+        // colocar a "água" no tabuleiro
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
                 matriz[i][j] = "~";
@@ -29,12 +37,13 @@ public class batalhaNaval {
         // gerador de número aleatorio para colocar navios
         Random gerador = new Random();
         for (int i = 0; i < 10; i++) {
-            int numeroAleatorio1 = gerador.nextInt(8);
+            int numeroAleatorio1 = gerador.nextInt(8); // pega os números de 0 a 7
             int numeroAleatorio2 = gerador.nextInt(8);
 
             // colocar os navios nas posições aleatorias
             if (matriz[numeroAleatorio1][numeroAleatorio2].contains("~")) { // if para não colocar navios na mesma posição
                 matriz[numeroAleatorio1][numeroAleatorio2] = "N";
+
             } else { // caso forem colocados na mesma posição refaz a tentativa
                 i--;
             }
@@ -43,38 +52,95 @@ public class batalhaNaval {
         return matriz;
     }
 
-    private void Escrever(String matriz[][]) {
-        // escrever o tabuleiro
-        System.out.println(" 0 1 2 3 4 5 6 7"); // casas eixo x
-        for (int i = 0; i < matriz.length; i++) { // for de linhas
-            System.out.print(i); // casa eixo y
-            for (int j = 0; j < matriz.length; j++) { // for de colunas
-                if (j == 7) { // if para quebrar linha no ultimo valor
-                    if (matriz[i][j].contains("N")) { // if para não mostrar os navios ao jogador
-                        System.out.println("~");
+    private void Escrever(String matriz[][], boolean continuar) {
+
+        if (continuar == true) { // se o jogo continuar será imprimido sem o local dos navios
+            // escrever o tabuleiro
+            System.out.println(" 0 1 2 3 4 5 6 7"); // casas eixo x
+            for (int i = 0; i < matriz.length; i++) { // for de linhas
+
+                System.out.print(i); // casa eixo y
+
+                for (int j = 0; j < matriz.length; j++) { // for de colunas
+
+                    if (j == 7) { // if para quebrar linha no ultimo valor
+
+                        if (matriz[i][j].contains("N")) { // if para não mostrar os navios ao jogador
+                            System.out.println("~");
+                        } else {
+                            System.out.println(matriz[i][j]);
+                        }
+
                     } else {
-                        System.out.println(matriz[i][j]);
-                    }
-                } else {
-                    if (matriz[i][j].contains("N")) { // if para não mostrar navios ao jogador
-                        System.out.print("~ ");
-                    } else {
-                        System.out.print(matriz[i][j]+" ");
+
+                        if (matriz[i][j].contains("N")) { // if para não mostrar navios ao jogador
+                            System.out.print("~ ");
+                        } else {
+                            System.out.print(matriz[i][j]+" ");
+                        }
                     }
                 }
             }
         }
         
-        System.out.println("\n 0 1 2 3 4 5 6 7"); // casas eixo x
-        for (int i = 0; i < matriz.length; i++) { // for de linhas
-            System.out.print(i); // casa eixo y
-            for (int j = 0; j < matriz.length; j++) { // for de colunas
-                if (j == 7) { // if para quebrar linha no ultimo valor
-                    System.out.println(matriz[i][j]);
-                } else {
-                     System.out.print(matriz[i][j]+" ");
+        if (continuar == false) { // se acabar será imprimido com o local dos navios
+            System.out.println("\n 0 1 2 3 4 5 6 7"); // casas eixo x
+            for (int i = 0; i < matriz.length; i++) { // for de linhas
+    
+                System.out.print(i); // casa eixo y
+    
+                for (int j = 0; j < matriz.length; j++) { // for de colunas
+    
+                    if (j == 7) { // if para quebrar linha no ultimo valor
+                        System.out.println(matriz[i][j]);
+                    } else { // para continuar na mesma linha
+                         System.out.print(matriz[i][j]+" ");
+                    }
                 }
-            }
+            }    
         }
+    }
+
+    private void Jogar(String matriz[][], int fim[]) {
+        Scanner s = new Scanner(System.in); // criação do scanner (não fechei por que da problema de execução)
+        System.out.println("Informe as coordenas (linha coluna):");
+        // inputs do jogador
+        int linha = s.nextInt();
+        int coluna = s.nextInt();
+        
+        // verificação dos valores
+        if (linha >= 0 && linha <= 7 && coluna >= 0 && coluna <= 7) {
+
+            if (matriz[linha][coluna].contains("N")) { // verifica se a casa tem navio
+                matriz[linha][coluna] = "X"; // coloca o valor de acerto em navio
+                System.out.println("Navio acertado!"); // informa que um navio foi acertado
+                fim[0]++; // acrescenta 1 em acertos
+                fim[1]++; // acrescenta 1 em jogadas
+
+            } else if (matriz[linha][coluna].contains("X") || matriz[linha][coluna].contains("O")) { // verifica se a casa já foi atacada
+                System.out.println("Casa já atacada"); // informa que a casa já foi atacada
+
+            } else { // se um navio não foi acertado
+                matriz[linha][coluna] = "O"; // coloca o valor de erro
+                System.out.println("Errou!"); // informa que não acertou
+                fim[1]++; // acrescenta 1 em jogadas 
+            }
+
+        } else { // se os valores não forem corretos
+            System.out.println("Posição inválida!!!!!!");
+        }
+    }
+
+    private boolean Fim(int fim[]) {
+        if (fim[0] == 10) { // se o jogador acertos os 10 navios ele ganha
+            System.out.println("Jogador ganhou!!!!!");
+            return false;
+
+        } else if (fim[1] == 30) { // se o jogador jogar 30 ele perde
+            System.out.println("Jogador perdeu!");
+            return false;
+        }
+
+        return true;
     }
 }
